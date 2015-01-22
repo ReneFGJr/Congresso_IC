5e6480c9f0970f71c521c88bbe6ae98e7b543c5c
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2015-01-22 19:01:22 -0200

    Update Cores

--- gadget/menu/windows/menu.php
+++ gadget/menu/windows/menu.php
@@ -2,63 +2,63 @@
 <script type="text/javascript" src="js/menu_windows.js"></script>
 <link rel="stylesheet" type="text/css" href="css/menu_windows.css">
 
-<h1>Início</h1>
+<h1 class="fcor00">Grupos</h1>
 
 <div class="pagina">
 	<div class="linha">
-		<div class="tile amarelo">
+		<div class="tile cor01">
 			<span class="titulo">Tile 1</span>
 			<br/>
 		</div>
-		<div class="tile azul">
+		<div class="tile cor01">
 			1
 		</div>
-		<div class="tile tileLargo vermelho">
+		<div class="tile tileLargo cor01">
 			2
 		</div>
-		<div class="tile verde">
+		<div class="tile cor01">
 			3
 		</div>
-		<div class="tile tileLargo amarelo">
+		<div class="tile tileLargo cor01">
 			4
 		</div>
-		<div class="tile verde">
+		<div class="tile cor01">
 			3
 		</div>		
-		<div class="tile tileLargo amarelo">
+		<div class="tile tileLargo cor01">
 			5
 		</div>
 		<div class="tile2x2">
-			<div class="tile tileMini verde">
+			<div class="tile tileMini cor01">
 				3
 			</div>
-			<div class="tile tileMini verde">
+			<div class="tile tileMini cor01">
 				3
 			</div>
-			<div class="tile tileMini verde">
+			<div class="tile tileMini cor01">
 				3
 			</div>
-			<div class="tile tileMini verde">
+			<div class="tile tileMini cor01">
 				3
 			</div>
 		</div>
-		<div class="tile azul">
+		<div class="tile cor01">
 			6
 		</div>
-		<div class="tile verde">
+		<div class="tile cor01">
 			7
 		</div>
-		<div class="tile vermelho">
+		<div class="tile cor01">
 			8
 		</div>
 			
-		<div class="tile azul">
+		<div class="tile cor01">
 			6
 		</div>
-		<div class="tile verde">
+		<div class="tile cor01">
 			7
 		</div>
-		<div class="tile vermelho">
+		<div class="tile cor01">
 			8
 		</div>
 
